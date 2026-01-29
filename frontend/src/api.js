55a532e12823072b6422aa3e633b9d79506e3fa9
HIMANSHU-KUSHWAHA-2004 <hk5652990@gmail.com>
2026-01-29 15:40:44 +0530

    s

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://auth-backend.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const registerUser = (data) => axios.post(`${API_URL}/auth/register`, data);
 export const loginUser = (data) => axios.post(`${API_URL}/auth/login`, data);
-export const submitForm = (data) => axios.post(`${API_URL}/submit`, data);
+export const submitForm = (data) => axios.post(`${API_URL}/form/submit`, data);
