eb1c092f8a5861c0f701a2dc42293f5a898ce863
HIMANSHU-KUSHWAHA-2004 <hk5652990@gmail.com>
2026-01-29 14:56:00 +0530

    d

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 import os
 
 # Supabase project URL and anon key
-SUPABASE_URL = "https://cgfkbomcjsyrvuwlehck.supabase.co"
-SUPABASE_KEY = "sb_publishable_hM1TLN49cr0MO3Wzza-USg_Sh3Vzhc0"
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 # Flask secret key for sessions
 SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
